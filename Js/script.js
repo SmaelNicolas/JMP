7d44abb81bae3fbb803badaaa5f403450987205a
Smael Nicolas <nico.smael@gmail.com>
2022-10-15 13:50:05 -0300

    remove swiper , add coutrnyimport

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
+import { addCountryToSelector } from "./domManipulation/addCountryToSelector.js";
 import { boxSearch } from "./domManipulation/boxSearch.js";
-import { startSwiperDownloads } from "./domManipulation/swiper.js";
 
+addCountryToSelector();
 boxSearch();
-startSwiperDownloads();
