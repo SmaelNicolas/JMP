ce910432eaff75b7222d731095ef88b38ba71cb8
Smael Nicolas <nico.smael@gmail.com>
2022-10-30 15:12:01 -0300

    add logic for search by list of countries

---
+++ Js/functions/searchByCountries.js
@@ -0,0 +1,61 @@
+import { renderSearchMessage } from "./renderSearchMessage.js";
+import { getUniversitiesSearched } from "./searchUniversities.js";
+
+export const searchByCountries = () => {
+	let dataUniversities = [];
+	fetch("./exampleKeywords.json")
+		.then((res) => res.json())
+		.then((res) => (dataUniversities = res));
+
+	const selectorCountries = document.getElementById("travelSearchSelector");
+	const selectorCountriesList = document.getElementById(
+		"travelBoxSearchListCountriesContainer"
+	);
+	const closeIcon = document.getElementById("closeListCountries");
+
+	const countryItems = document.getElementsByClassName(
+		"travelBoxSearchListCountriesItem"
+	);
+	const arrayCountryItems = Array.from(countryItems);
+
+	let arrayCountries = [];
+
+	selectorCountries.addEventListener("click", () => {
+		selectorCountriesList.classList.toggle(
+			"ListCountriesContainerHideShow"
+		);
+	});
+
+	closeIcon.addEventListener("click", () => {
+		selectorCountriesList.classList.toggle(
+			"ListCountriesContainerHideShow"
+		);
+	});
+
+	arrayCountryItems.map((item) => {
+		item.addEventListener("click", () => {
+			item.classList.toggle("travelBoxSearchListCountriesItemSelected");
+			if (
+				item.classList.contains(
+					"travelBoxSearchListCountriesItemSelected"
+				)
+			) {
+				arrayCountries.push(item.getAttribute("id"));
+				getUniversitiesSearched(
+					arrayCountries.join(" "),
+					dataUniversities
+				);
+			} else {
+				arrayCountries = arrayCountries.filter(
+					(country) => country !== item.getAttribute("id")
+				);
+				arrayCountries.length > 0
+					? getUniversitiesSearched(
+							arrayCountries.join(" "),
+							dataUniversities
+					  )
+					: renderSearchMessage();
+			}
+		});
+	});
+};
